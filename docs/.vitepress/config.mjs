88856523e1ad95776c35cdd72fd8bad3f51f9a8f
iannC <solwolfs@gmail.com>
2025-04-07 17:09:04 +0300

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo: '/elitegamers.gif',
+    logo: '/elitegamers.webp',
     search: {
       provider: 'local',
       options: {
